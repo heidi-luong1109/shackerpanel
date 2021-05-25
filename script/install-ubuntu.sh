915218912cffa0b4ef98237390df5ac914efe644
heidi-luong1109 <heidi.luong1109@yahoo.com>
2021-05-26 08:22:52 +1000

    fix

--- script/install-ubuntu.sh
+++ script/install-ubuntu.sh
@@ -747,6 +747,5 @@ echo -e "=================================================================="
 endTime=`date +%s`
 ((outTime=($endTime-$startTime)/60))
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
-rm -f install-ubuntu_6.0_en.sh
 
 
